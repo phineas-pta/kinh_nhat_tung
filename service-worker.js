b9ac8f11343b17405c87f2284150606cc13dfd27
phineas-pta <phantuananh@hotmail.com.vn>
2021-10-31 01:29:22 +0200

    service worker: workbox

--- service-worker.js
+++ service-worker.js
@@ -3,3 +3,19 @@ for (let i of ['install', 'activate', 'fetch']) {
 		console.log('[ServiceWorker] ' + i);
 	});
 }
+
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+
+workbox.routing.registerRoute(
+	new RegExp('/*'),
+	new workbox.strategies.StaleWhileRevalidate({
+		cacheName: 'offline'
+	})
+);
+
+workbox.routing.registerRoute(
+	new RegExp('/*'),
+	new workbox.strategies.CacheFirst({
+		cacheName: 'precache'
+	})
+);
