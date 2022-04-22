1a59b81a8f4370fd04c481cb76908fcc80a51718
phineas-pta <phantuananh@hotmail.com.vn>
2022-04-22 23:35:43 +0200

    script: unescape HTML entities for Chinese characters

--- assets/aide_de_camp.py
+++ assets/aide_de_camp.py
@@ -72,13 +72,17 @@ def combo(textHan, textViet, esc = True, printed = True, debug = False):
 		if debug: print("ckpt3:", x)
 		y = escapeHTML(x) if esc else x
 		if x in Han_punc:
-			res += f"<ruby><rb>{y}</rb><rt></rt></ruby>\n"
+			res += f"<ruby><rb>{y}</rb>"
+			res += f"<!-- {x} -->" if esc else ""
+			res += "<rt></rt></ruby>\n"
 			j += 1
 		else:
 			if debug: print("ckpt4:", test3[i])
 			if x != test3[i]: raise ValueError("punctuation error")
 			if debug: print("ckpt5:", textViet_[i])
-			res += f"<ruby><rb>{y}</rb><rt>{textViet_[i]}</rt></ruby>\n"
+			res += f"<ruby><rb>{y}</rb>"
+			res += f"<!-- {x} -->" if esc else ""
+			res += f"<rt>{textViet_[i]}</rt></ruby>\n"
 			i += 1
 
 	if printed: print(res)
@@ -108,19 +112,31 @@ while True:
 
 # %% batch escape/unescape HTML & unicode entities
 
+test_txt = "<ruby><rb>&#21335;</rb><rt>Nam</rt></ruby><ruby><rb>&#28961;</rb><rt>mô</rt></ruby>"
+
 ruby_base = re.compile(r"(?<=<rb>)[^<]+(?=</rb>)")
-" ".join(map(
+" ".join(map( # example
 	html.unescape,
-	ruby_base.findall("<ruby><rb>&#21335;</rb><rt>Nam</rt></ruby><ruby><rb>&#28961;</rb><rt>mô</rt></ruby>")
+	ruby_base.findall(test_txt)
 ))
 
-basepath = "pathtodir/DataFiles/"
 esc_fn = lambda x: escapeHTML(x.group(0))
 ruby_sub = lambda txt: ruby_base.sub(esc_fn, txt)
+
+han_ruby = re.compile(r"<rb>&#[^7]\d+;</rb>")
+def unesc_han_fn(matchobj):
+	x = matchobj.group(0)[4:-5] # because len("<rb>") = 4 and len("</rb>") = 5
+	y = html.unescape(x)
+	return f"<rb>{x}</rb><!-- {y} -->"
+han_ruby.sub(unesc_han_fn, test_txt) # example
+unesc_han_sub = lambda txt: han_ruby.sub(unesc_han_fn, txt)
+
+basepath = "pathtodir/DataFiles/"
 for filename in os.listdir(basepath):
 	print(filename)
 	if filename.endswith(".html"):
-		with open(basepath + filename, mode = "r", encoding = "utf-8") as file:
+		my_file = basepath + "/" + filename
+		with open(my_file, mode = "r", encoding = "utf-8") as file:
 			tmp = file.readlines()
-		with open(basepath + filename, mode = "w", encoding = "utf-8") as file:
-			file.writelines(map(ruby_sub, tmp)) # doesn't need list object
+		with open(my_file, mode = "w", encoding = "utf-8") as file:
+			file.writelines(map(unesc_han_sub, tmp)) # doesn't need list object
