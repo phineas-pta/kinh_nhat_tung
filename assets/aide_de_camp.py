28cf974694bcb947aa380abb6b2cb19fd5317ada
phineas-pta <phantuananh@hotmail.com.vn>
2023-01-15 16:39:09 +0100

    Chuyển đổi Hán-Việt với thivien.net API

--- assets/aide_de_camp.py
+++ assets/aide_de_camp.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 
-import re, os, html, requests as req
+import re, os, html, json, requests as req
 from unicodedata import normalize as uni_norm
 
-def escapeHTML(txt):
+def escapeHTML(txt: str) -> str:
 	"""transform Unicode character -> DEC numerical entity"""
 	return txt.encode("ascii", "xmlcharrefreplace").decode()
 
@@ -13,7 +13,7 @@ escapeHTML(uni_norm("NFD","đức")) # decomposed form
 
 # %%
 
-def pali(textDeva, textLatn, esc = True):
+def pali(textDeva: str, textLatn: str, esc: bool=True) -> str:
 	textLatn_, textDeva_ = textLatn.split(" "), textDeva.split(" ") # split each word
 	if esc: textDeva_ = list(map(escapeHTML, textDeva_))
 	if len(textDeva_) != len(textLatn_): raise ValueError
@@ -22,15 +22,17 @@ def pali(textDeva, textLatn, esc = True):
 		res += f"<ruby><rb>{textDeva_[i]}</rb><rt>{textLatn_[i]}</rt></ruby>\n"
 	return res
 
+headers = {"User-Agent": "kinh_nhat_tung/6.3.2 (https://github.com/phineas-pta/kinh_nhat_tung) Python/3.11"}
+
 # API conversion multiple scripts
-baseurl = "https://aksharamukha-plugin.appspot.com/api/public"
-reqdict = dict(source = "IAST", target = "Siddham") # "IAST", "IPA", "ISO", "Devanagari"
-def toSiddham(textIAST, ruby = True, esc = True):
+aksha_url = "https://aksharamukha-plugin.appspot.com/api/public"
+aksha_reqdict = {"source": "IAST", "target": "Siddham"} # "IAST", "IPA", "ISO", "Devanagari"
+def toSiddham(textIAST: str, ruby: bool=True, esc: bool=True) -> str:
 	"""convert romanized text to Siddham script"""
 	# some typo when copied from Digital Sanskrit Buddhist
 	textIASTbis = textIAST.replace("|", ".").replace(" .", ".").replace(" ?", "?")
-	reqdict["text"] = textIASTbis
-	textSidd = req.get(baseurl, params = reqdict).text
+	aksha_reqdict["text"] = textIASTbis
+	textSidd = req.get(aksha_url, headers=headers, params=aksha_reqdict).text
 	if ruby: # replace after to have special Siddham punctuation
 		res = pali(textSidd, textIASTbis.replace("..", "."), esc)
 	else:
@@ -40,9 +42,9 @@ def toSiddham(textIAST, ruby = True, esc = True):
 
 while True: print(toSiddham(input("text IAST: ")), "\n")
 
-def stanzas(textIAST, esc = True, printed = True):
+def stanzas(textIAST: str, esc: bool=True, printed: bool=True):
 	"""stanzas of Siddham"""
-	textSidd = toSiddham(textIAST, ruby = False)
+	textSidd = toSiddham(textIAST, ruby=False)
 	textIAST_, textSidd_ = textIAST.split("\n"), textSidd.split("\n")
 	res = ""
 	for i in range(len(textIAST_)):
@@ -53,10 +55,22 @@ def stanzas(textIAST, esc = True, printed = True):
 
 # %%
 
+thivien_url = "https://hvdic.thivien.net/transcript-query.json.php"
+thivien_headers = {"Content-Type": "application/x-www-form-urlencoded; charset=UTF-8", **headers}
+
+# i also shared this code at https://gist.github.com/phineas-pta/457b9f546ec20d5d2019d5799847eb01
+def convertHanViet(textHan: str, printed: bool=True) -> str:
+	payload = f"mode=trans&lang=1&input={textHan}"
+	response = requests.request("POST", thivien_url, headers=thivien_headers, data=payload.encode())
+	result = json.loads(response.text)["result"]
+	res = " ".join([el["o"][0] for el in result])
+	if printed: print(res)
+	else: return res
+
 Han_punc = list("，、：；．。！？…⋯～／‧•●『』「」（）《》〈〉［］【】〖〗〔〕｛｝")
 punc_search = re.compile("(" + "|".join(Han_punc) + ")")
 
-def combo(textHan, textViet, esc = True, printed = True, debug = False):
+def combo(textHan: str, textViet: str, esc: bool=True, printed: bool=True, debug: bool=False) -> str:
 	test0 = textHan.replace(" ", "") # remove spaces
 	test1 = punc_search.sub("", test0) # without punc
 	test2, test3 = list(test0), list(test1) # split each character: with & without punc
@@ -90,7 +104,7 @@ def combo(textHan, textViet, esc = True, printed = True, debug = False):
 
 while True: print(combo(input("text Hant: "), input("text Viet: ")), "\n")
 
-def verse_HanViet(textHan, textViet, esc = True, printed = True):
+def verse_HanViet(textHan: str, textViet: str, esc: bool=True, printed: bool=True) -> str:
 	"""combine text (multiple lines) into ruby annotation in HTML"""
 	text1, text2 = textHan.split("\n"), textViet.split("\n")
 	res = ""
@@ -107,7 +121,7 @@ while True:
 	print()
 	print(toSiddham(textIAST))
 	print()
-	print(combo(textHan, textViet, printed = False))
+	print(combo(textHan, textViet, printed=False))
 	print()
 
 # %% batch escape/unescape HTML & unicode entities
@@ -124,7 +138,7 @@ esc_fn = lambda x: escapeHTML(x.group(0))
 ruby_sub = lambda txt: ruby_base.sub(esc_fn, txt)
 
 han_ruby = re.compile(r"<rb>&#[^7]\d+;</rb>")
-def unesc_han_fn(matchobj):
+def unesc_han_fn(matchobj: re.Match) -> str:
 	x = matchobj.group(0)[4:-5] # because len("<rb>") = 4 and len("</rb>") = 5
 	y = html.unescape(x)
 	return f"<rb>{x}</rb><!-- {y} -->"
@@ -136,7 +150,7 @@ for filename in os.listdir(basepath):
 	print(filename)
 	if filename.endswith(".html"):
 		my_file = basepath + "/" + filename
-		with open(my_file, mode = "r", encoding = "utf-8") as file:
+		with open(my_file, mode="r", encoding="utf-8") as file:
 			tmp = file.readlines()
-		with open(my_file, mode = "w", encoding = "utf-8") as file:
+		with open(my_file, mode="w", encoding="utf-8") as file:
 			file.writelines(map(unesc_han_sub, tmp)) # doesn't need list object
